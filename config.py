b3af8fb94ca6a196dab3a44c751137d117a2a650
Priya22115 <lknnkumar5@gmail.com>
2024-08-02 09:51:56 +0530

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("dhdhdhhdudj")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
