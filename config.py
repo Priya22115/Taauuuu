c50df48c7b05adbfaffdd30f913357cd10efa66c
Priya22115 <lknnkumar5@gmail.com>
2024-08-03 21:38:38 +0530

    config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("dhdhdhhdudj")
+BOT_TOKEN=("7439277702:AAGQ9yjvcUj6iCIAMfWmmRUTV1omrTs96Oo")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("PRIYA_TG")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1291855508")
 
 
 
